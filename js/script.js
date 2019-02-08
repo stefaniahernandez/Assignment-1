49cada1d0be099fdbf322e6a113e0201e293beb0
stefaniahernandez <47236121+stefaniahernandez@users.noreply.github.com>
2019-02-07 21:54:17 -0800

    Assignment 1 - Stefania Hernandez

---
+++ js/script.js
@@ -0,0 +1,180 @@
+/*eslint-env browser*/
+
+//STEP 1//
+/*
+    var someMonth;
+    function theMonth;
+    var currentMonth;
+    var summerMonth;
+    var myLibraryFunction;
+*/
+//STEP 2//
+/*
+    7 //Numeric//
+    "Say something." //String//
+    false //Boolean//
+    null //Null//
+*/
+//STEP 3//
+/*
+    var somethingImportant = expressionOne + expressionTwo + "%";
+    var somethingMathamatical = 3 * 8 + (2/7);
+*/
+//STEP 4//
+/*
+    var firstName;
+    var lastName;
+    var address;
+    var city;
+    var state;
+    var numZipCode;
+    var numYourAge;
+    var strReferralSource;
+    var mayWeContactYou;
+*/
+//STEP 5//
+/*
+    var firstName;
+    var lastName;
+    var address;
+    var city;
+    var state;
+    var numZipCode;
+    var numYourAge;
+    var strReferralSource;
+    var mayWeContactYou;
+    firstName = "Stefania";
+    lastName ="Hernandez";
+    address = true;
+    city = true;
+    state = true;
+    numZipCode = 7;
+    numYourAge = 72;
+    strReferralSource = true;
+    mayWeContactYou = true;
+    
+    var firstName = "Stefania";
+    var lastName = "Hernandez";
+    var address = true;
+    var city = true;
+    var state = true;
+    var numZipCode = 7;
+    var numYourAge = 72;
+    var strReferralSource = true;
+    var mayWeContactYou = true;
+
+    var firstName = "Stefania", lastName = "Hernandez", address = true, city = true, state = true, numZipCode = 7, numYourAge = 7, strReferralSource = true, mayWeContactYou = true;
+*/
+//STEP 6//
+/*
+    var years = 7;
+    window.console.log("It takes " + years + " years to learn how to code.");
+*/
+//STEP 7//
+/*
+    var firstName = true + "Stefania";
+    window.console.log(firstName);
+
+    var yourAge = 7 + true;
+    window.console.log(yourAge);
+*/
+//STEP 8//
+/*
+    var someString = "Who once said, " + "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.";
+    window.console.log(someString);
+*/
+//STEP 9//
+/*
+    var someThing = null;
+    window.console.log(someThing);
+
+    var someTimes;
+    window.console.log(someTimes);
+*/
+//STEP 10//
+/*
+    typeof "Nia" // Returns string
+    typeof 77    // Returns number
+    typeof false // Returns Boolean
+    typeof new() // Returns object
+    typeof inPut // Returns undefined
+*/
+//STEP 11//
+/*
+    window.alert("Hello Stefania," + " welcome to the JavaScript class!");
+*/
+//STEP 12//
+/*
+    var name = "Stefania Hernandez";
+    window.alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+//STEP 13//
+/*
+    var name = "Stefania Hernandez";
+    var course = "JavaScript";
+    window.alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+//STEP 14//
+/*
+    var name = "Stefania Hernandez";
+    var course = "JavaScript";
+    window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+//STEP 15//
+/*
+    var name = prompt("What is your name?");
+    var course = "JavaScript";
+    window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+//STEP 16//
+/*
+    var course = prompt("What class are you taking?");
+    window.alert("Welcome to the " + course + " class!");
+*/
+//STEP 17//
+/*
+    var x = 10;
+    var y = 20;
+    window.console.log(x + y);
+*/
+//STEP 18//
+/*
+    var x = 20;
+    window.console.log(x + 20);
+*/
+//STEP 19//
+/*
+    var x = 20;
+    window.console.log(x * 5);
+*/
+//STEP 20//
+/*
+    var x = 20%3;
+    window.console.log(x);
+*/
+//STEP 21//
+/*
+    var x = 5;
+    var y = 2;
+    window.console.log(x > y);
+*/
+//STEP 22//
+/*
+    var x = 99;
+    var y = 9;
+    window.console.log(x == y);
+*/
+//STEP 23//
+/*
+    var widget = new Object();
+    window.console.log(typeof widget);
+*/
+//STEP 24//
+/*
+    var widget = new Object();
+    window.console.log(widget instanceof Object);
+*/
+//STEP 25//
+    
+   
+
